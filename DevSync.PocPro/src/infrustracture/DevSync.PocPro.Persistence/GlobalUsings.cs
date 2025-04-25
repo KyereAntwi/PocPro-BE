55cfa943ce764abba564094d1593cb4bbfe71b14
Joseph Antwi <joseph.kyereantwi@heidelbergmaterials.com>
2025-04-25 16:00:16 +0000

    Added needed services to aspire and api

---
+++ DevSync.PocPro/src/infrustracture/DevSync.PocPro.Persistence/GlobalUsings.cs
@@ -0,0 +1,6 @@
+// Global using directives
+
+global using System.Security.Claims;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.Extensions.DependencyInjection;
